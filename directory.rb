c735c8daf888c33bf112fd42e2d9a0ec57c29182
Stefanie Lyons <stefanie.hodges@yahoo.co.uk>
2016-12-11 21:17:42 +0000

    load student data from command line

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -12,6 +12,7 @@ def print_menu# 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -31,6 +32,8 @@ def process(selection)
     show_students()
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -44,15 +47,15 @@ def input_students
 # Create an empty array
   cohort_months = ["January", "March", "May", "July", "September", "November"]
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     puts "Which cohort do they belong to?"
-    cohort = handle_default(gets.chomp)
+    cohort = handle_default(STDIN.gets.chomp)
     puts "What is the student's main hobby?"
-    hobby = handle_default(gets.chomp)
+    hobby = handle_default(STDIN.gets.chomp)
     puts "Where is the student's hometown?"
-    hometown = handle_default(gets.chomp)
+    hometown = handle_default(STDIN.gets.chomp)
     # add the student hash to the array
     if cohort_months.include?(cohort)
     @students << {name: name, cohort: cohort, hobby: hobby, hometown: hometown}
@@ -65,7 +68,7 @@ def input_students
     else
     puts "A valid cohort was not entered, please type another."
   end
-  name = gets.chomp
+  name = STDIN.gets.chomp
 end
 
 end
@@ -82,6 +85,30 @@ def save_students
   end
   file.close
 end
+
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+    else # if it doesn't exist
+      puts "Sorry #{filename} doesn't exist."
+      exit # quit the programme
+    end
+  end
+
 
 
 def handle_default(answer)
@@ -108,6 +135,7 @@ def print_footer()
   puts "Overall, we have #{@students.count} great students".center(50)
 end
 
+try_load_students
 interactive_menu
 # nothing happens until we call the methods
 if @students.count == 0
